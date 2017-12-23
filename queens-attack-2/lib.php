d446b61b5d6f0230315dd391669d0016d4808742
Batov <i.batov@megaplan.ru>
2017-12-23 14:33:30 +0300

    queen-attack-2

--- queens-attack-2/lib.php
+++ queens-attack-2/lib.php
@@ -14,19 +14,19 @@ class Process
        * add border obstacles
        */
       // left border
-      for ($i=-1; $i<$n+1; $i++) {
+      for ($i=-1; $i<=$n; $i++) {
         $obstacles[] = [-1, $i];
       }
       // top border
-      for ($i=-1; $i<$n+1; $i++) {
-        $obstacles[] = [$i, $n+1];
+      for ($i=-1; $i<=$n; $i++) {
+        $obstacles[] = [$i, $n];
       }
       // right border
-      for ($i=-1; $i<$n+1; $i++) {
-        $obstacles[] = [$n+1, $i];
+      for ($i=-1; $i<=$n; $i++) {
+        $obstacles[] = [$n, $i];
       }
       // bottom border
-      for ($i=-1; $i<$n+1; $i++) {
+      for ($i=-1; $i<=$n; $i++) {
         $obstacles[] = [$i, -1];
       }
 
@@ -37,50 +37,50 @@ class Process
       }
 
       // calc obstacles that lies on diagonals or axes
-      $obstDistances = [INF, INF, INF, INF, INF, INF, INF]; // indexed from top-left half-diagonal to bottom-right, clockwise
+      $obstDistances = [INF, INF, INF, INF, INF, INF, INF, INF]; // indexed from top-left half-diagonal to bottom-right, clockwise
       foreach ($obstacles as $i => $obstacle) {
           if ($obstacle[0] < 0 && $obstacle[1] > 0 && abs($obstacle[0]) === abs($obstacle[1])) {
               // means it lies diagonal in first quadrant diag, calculate distance to zero
-              if ($obstacle[1] - 2 < $obstDistances[0]) {
-                  $obstDistances[0] = $obstacle[1] - 2;
+              if ($obstacle[1] - 1 < $obstDistances[0]) {
+                  $obstDistances[0] = $obstacle[1] - 1;
               }
           }
           if ($obstacle[1] > 0 && $obstacle[0] === 0) {
               // means it lies on up axes, calculate distance to zero
-              if ($obstacle[1] - 2 < $obstDistances[1]) {
-                  $obstDistances[1] = $obstacle[1] - 2;
+              if ($obstacle[1] - 1 < $obstDistances[1]) {
+                  $obstDistances[1] = $obstacle[1] - 1;
               }
           }
           if ($obstacle[0] > 0 && $obstacle[1] > 0 && abs($obstacle[0]) === abs($obstacle[1])) {
               // means it lies diagonal in second quadrant diagonal, calculate distance to zero
-              if ($obstacle[1] - 2 < $obstDistances[2]) {
-                  $obstDistances[2] = $obstacle[1] - 2;
+              if ($obstacle[1] - 1 < $obstDistances[2]) {
+                  $obstDistances[2] = $obstacle[1] - 1;
               }
           }
           if ($obstacle[0] > 0 && $obstacle[1] === 0) {
               // means it lies n right axes, calculate distance to zero
-              if ($obstacle[0] - 2 < $obstDistances[3]) {
-                  $obstDistances[3] = $obstacle[0] - 2;
+              if ($obstacle[0] - 1 < $obstDistances[3]) {
+                  $obstDistances[3] = $obstacle[0] - 1;
               }
           }
           if ($obstacle[0] > 0 && $obstacle[1] < 0 && abs($obstacle[0]) === abs($obstacle[1])) {
-              if (abs($obstacle[0]) - 2 < $obstDistances[4]) {
-                  $obstDistances[4] = abs($obstacle[0]) - 2;
+              if (abs($obstacle[0]) - 1 < $obstDistances[4]) {
+                  $obstDistances[4] = abs($obstacle[0]) - 1;
               }
           }
           if ($obstacle[0] === 0 && $obstacle[1] < 0) {
-              if (abs($obstacle[1]) - 2 < $obstDistances[5]) {
-                  $obstDistances[5] = abs($obstacle[1]) - 2;
+              if (abs($obstacle[1]) - 1 < $obstDistances[5]) {
+                  $obstDistances[5] = abs($obstacle[1]) - 1;
               }
           }
           if ($obstacle[0] < 0 && $obstacle[1] < 0 && abs($obstacle[0]) === abs($obstacle[1])) {
-              if (abs($obstacle[0]) - 2 < $obstDistances[6]) {
-                  $obstDistances[6] = abs($obstacle[0]) - 2;
+              if (abs($obstacle[0]) - 1 < $obstDistances[6]) {
+                  $obstDistances[6] = abs($obstacle[0]) - 1;
               }
           }
           if ($obstacle[0] < 0 && $obstacle[1] === 0) {
-              if (abs($obstacle[1]) - 2 < $obstDistances[7]) {
-                  $obstDistances[7] = abs($obstacle[1]) - 2;
+              if (abs($obstacle[0]) - 1 < $obstDistances[7]) {
+                  $obstDistances[7] = abs($obstacle[0]) - 1;
               }
           }
         }
@@ -101,13 +101,13 @@ class Process
         $n = null;
 
         list ($n, $k) = $this->scanf($handle, "%d %d");
-        list ($qx, $qy) = $this->scanf($handle, "%d %d");
+        list ($qy, $qx) = $this->scanf($handle, "%d %d");
         $obstacles = [];
         for ($i=0; $i<$k; $i++) {
-            list ($x, $y) = $this->scanf($handle, "%d %d");
-            $obstacles[] = [$x, $y];
+            list ($y, $x) = $this->scanf($handle, "%d %d");
+            $obstacles[] = [$x-1, $y-1]; // -1 because we want to count from zero
         }
-        return $this->p($n, [$qx, $qy], $obstacles);
+        return $this->p($n, [$qx-1, $qy-1], $obstacles);
     }
 
     function scanf(&$handle, $format)
--- queens-attack-2/test.php
+++ queens-attack-2/test.php
@@ -4,9 +4,12 @@
     $p->setDebug(true);
 
     $s = fopen('test_data/test1.txt', 'r');
-    var_dump($p->run($s)); echo ' must be '; var_dump('9');
+    //var_dump($p->run($s)); echo ' must be '; var_dump('9');
 
     $s = fopen('test_data/test2.txt', 'r');
-    var_dump($p->run($s)); echo ' must be '; var_dump('10');
+//    var_dump($p->run($s)); echo ' must be '; var_dump('10');
+
+    $s = fopen('test_data/test3.txt', 'r');
+    var_dump($p->run($s)); echo ' must be '; var_dump('307303');
 
 
